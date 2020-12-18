26775c3d1857079eb86d50ac29aecd3fe49ddfef
gisu.kim <markiron99@gmail.com>
2020-12-18 12:22:18 +0900

    boomdoggy signin/signup endpoint complete

--- users/models.py
+++ users/models.py
@@ -1,4 +1,7 @@
-from django.db import models
+from    django.db   import models
+import  validation  
+validate_phone = validation.validate_phone
+validate_city = validation.validate_city
 
 class Users(models.Model):
     first_name      =   models.CharField(max_length=100)
@@ -13,10 +16,17 @@ class Users(models.Model):
     def __str__(self):
         return self.name
 
-
+#로그인 후 마이페이지 내에 있는 고객 주소 관리
 class Addresses(models.Model):
-    user            =   models.ForeignKey("Users", on_delete = models.CASCADE)
-    address         =   models.CharField(max_length = 200)
+    user                =   models.ForeignKey('Users', on_delete = models.CASCADE)
+    first_name          =   models.CharField(max_length = 200)
+    last_name           =   models.CharField(max_length = 200)
+    address             =   models.CharField(max_length = 500)
+    appartment_type     =   models.CharField(max_length = 500)
+    city                =   models.CharField(max_length = 200, validators=[validate_city], blank = False)
+    county_region       =   models.CharField(max_length = 100)
+    postcode            =   models.CharField(max_length = 50)
+    phone_number        =   models.CharField(max_length = 50, validators=[validate_phone], blank = False)
 
     class Meta:
         db_table = "address"
--- users/views.py
+++ users/views.py
@@ -1,3 +1,79 @@
-from django.shortcuts import render
+import  json, re
+import  bcrypt
+import  jwt
 
-# Create your views here.
+from    django.http                 import  JsonResponse
+from    django.views                import  View
+from    boomdoggy.settings          import  SECRET_KEY
+
+from    .models                     import  Users
+import  validation
+validate_phone = validation.validate_phone
+validate_city = validation.validate_city
+
+
+
+class SignUp(View) : 
+    def post(self, request) : 
+        data = json.loads(request.body)
+
+        #비밀번호 암호화
+        hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+        
+        #이메일, 비밀번호 유효성
+        REGEX_EMAIL         = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        REGEX_PASSWORD      = '^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$'
+
+        try : 
+            if not (data['first_name'] and data['last_name']) : 
+                return JsonResponse({'messages' : 'PLEASE_INSERT_YOUR_NAME'}, status = 400)
+
+            if not re.match(REGEX_EMAIL, data['email']) : 
+                return JsonResponse({'message' : 'INVALID_ID'}, status = 401)
+
+            if not re.match(REGEX_PASSWORD, data['password']) : 
+                return JsonResponse({'message' : 'INVALID_PASSWORD'}, status = 401)
+
+            if Users.objects.filter(email = data['email']).exists() : 
+                return JsonResponse({'message' : 'ALREADY_EXISTS'}, status = 401)
+
+            else : 
+                Users.objects.create(
+                        first_name  =   data['first_name'],
+                        last_name   =   data['last_name'],
+                        email       =   data['email'],
+                        password    =   hashed_password.decode('utf-8'),
+                        )
+
+                return JsonResponse({'message' : 'SUCCESS'}, status = 201)
+
+        except KeyError : 
+            return JsonResponse({'message' : 'INVALID_KEY'}, status = 400)
+
+
+class SignIn(View) : 
+
+    def post(self, request) : 
+        data        = json.loads(request.body)
+        email       = data.get('email')
+        password    = data.get('password')
+
+        
+        try : 
+            if Users.objects.filter(email = email).exists() : 
+                user = Users.objects.get(email = email)
+
+                if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')) : 
+                    access_token = jwt.encode(
+                            {'email': email}, SECRET_KEY, algorithm = 'HS256'
+                            ).decode('utf-8')
+
+                    return JsonResponse({'message' : 'SUCCESS', 'Token' : access_token}, status = 200)
+                return JsonResponse({'message' : 'PASSWORD_IS_NOT_VALID'}, status = 400)
+                    
+            else :  
+                return JsonResponse({'message' : "THIS_USER_DOES_NOT_EXIST"}, status = 401)
+
+            
+        except KeyError : 
+            return JsonResponse({'message' : 'INVALID_KEY'}, status = 400)
