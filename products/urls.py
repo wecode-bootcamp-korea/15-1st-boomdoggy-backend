03bac0f45025ddc9e5302c1b26d0f681fa0e10b5
Luceta <57941164+Luceta@users.noreply.github.com>
2020-12-21 02:00:30 +0900

    Add : food-list end point

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from products.views import TreatView
+from products.views import TreatView, FoodViews
 
 urlpatterns = [
     path('/our-treats',TreatView.as_view(), name ='our-treats'),
+    path('/our-food',FoodViews.as_view(), name ='our-food'),
 ]
--- products/views.py
+++ products/views.py
@@ -23,3 +23,19 @@ class TreatView(View):
             "sale_rate"     : treats[i].sale_rate.sale_rate}for i in range(len(treats))]
 
         return JsonResponse({"treats_list" : treats_list}, status=200)
+
+class   FoodView(View):
+    def get(self,request):
+        foods    = Products.objects.exclude(category_id =treats)
+
+        foods_list = [{
+            "id"            : foods[i].id,
+            "name"          : foods[i].name,
+            "main_image"    : Images.objects.filter(product_id = treats[i].id).first().image_url,
+            "sub_image"     : Images.objects.filter(product_id = treats[i].id).last().image_url,
+            "category"      : foods[i].categroy.name,
+            "price"         : foods[i].price,
+            "stock"         : foods[i].stock_status,
+            "sale_rate"     : foods[i].sale_rate.sale_rate} for i in range(len(foods))]
+
+        return JsonResponse({"foods_list":foods_list}, status=200)
