0e1e696fce4844cb32304b150c118f2c145d7246
Luceta <57941164+Luceta@users.noreply.github.com>
2020-12-21 00:02:04 +0900

    ADD product-list view: one category finished

--- products/views.py
+++ products/views.py
@@ -7,14 +7,19 @@ from products.models import Categories, Images, Products
 
 class TreatView(View):
     def get(self,request):
-        products    = Products.objects.all()
+        treats      = Categories.objects.get(name="treats")
         products    = Products.objects.filter(category_id = treats)
 
+        treats    = Products.objects.filter(category_id = treats)
 
-        treats_query    = products.filter(category_id = treats)
-
-
+        treats_list = [{
+            "id"            : treats[i].id,
+            "name"          : treats[i].name,
+            "main_image"    : Images.objects.filter(product_id = treats[i].id).first().image_url,
+            "sub_image"     : Images.objects.filter(product_id = treats[i].id).last().image_url,
+            "category"      : treats[i].category.name,
+            "price"         : treats[i].price,
+            "stock"         : treats[i].stock_status,
+            "sale_rate"     : treats[i].sale_rate.sale_rate}for i in range(len(treats))]
 
         return JsonResponse({"treats_list" : treats_list}, status=200)
-
-
